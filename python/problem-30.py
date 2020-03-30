c9a49a18fa9240f7a2eb9f972c82e8d4659f2567
Naveen Venkatesan <naveen.venkatesan@gmail.com>
2020-03-29 23:42:59 -0700

    Problem 30

---
+++ python/problem-30.py
@@ -0,0 +1,24 @@
+### Problem 30 - Digit Fifth Powers
+###-----------------------------------------------------------------------------------------------------------
+### Surprisingly there are only three numbers that can be written as the sum of fourth powers of their digits:
+### 1634 = 1^4 + 6^4 + 3^4 + 4^4
+### 8208 = 8^4 + 2^4 + 0^4 + 8^4
+### 9474 = 9^4 + 4^4 + 7^4 + 4^4
+### As 1 = 1^4 is not a sum it is not included.
+### The sum of these numbers is 1634 + 8208 + 9474 = 19316.
+### Find the sum of all the numbers that can be written as the sum of fifth powers of their digits.
+
+### Solution
+
+# 6*9^5 = 354294 which will set the upper limit
+
+sumFifthPowers = 0
+
+for i in range(2, 354295):
+	total = 0
+	for j in str(i):
+		total += int(j)**5
+	if total == i:
+		sumFifthPowers += i
+
+print('The sum of fifth power numbers is: ' + str(sumFifthPowers))
