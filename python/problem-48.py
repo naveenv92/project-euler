0088911f43d01d4a46069a28dec1798d728d0cf4
Naveen Venkatesan <naveen.venkatesan@gmail.com>
2020-03-30 00:05:36 -0700

    Problem 48

---
+++ python/problem-48.py
@@ -0,0 +1,13 @@
+### Problem 48 - Self Powers
+###----------------------------------------------------------------------
+### The series, 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317.
+### Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000
+
+### Solution
+
+selfPowerSum = 0
+
+for i in range(1, 1001):
+	selfPowerSum += i**i
+
+print('The last ten digits are: ' + str(selfPowerSum)[-10:])
