89cd94bbcaa44ee25f21ca0d7e2e8e1ce9aaf4cf
Naveen Venkatesan <naveen.venkatesan@gmail.com>
2020-04-06 16:58:25 -0700

    Problem 56

--- python/problem-56.py
+++ python/problem-56.py
@@ -1,4 +1,4 @@
-### Problem 97 - Large Non-Mersenne Prime
+### Problem 56 - Powerful Digit Sum
 ###---------------------------------------------------------------------------------------------------------------------------------------------------
 ### A googol (10^100) is a massive number: one followed by one-hundred zeros; 100^100 is almost unimaginably large: one followed by two-hundred zeros. 
 ### Despite their size, the sum of the digits in each number is only 1.
