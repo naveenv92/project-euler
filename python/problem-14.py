cffa0876676e05b2cd6a6d85d7b9c0adde18c5e5
Naveen Venkatesan <naveen.venkatesan@gmail.com>
2020-04-26 21:41:46 -0700

    Updated files

--- python/problem-14.py
+++ python/problem-14.py
@@ -1,39 +1,62 @@
-### Problem 14 - Longest Collatz Sequence
-###--------------------------------------------------------------------------------------------------------------------------
-### The following iterative sequence is defined for the set of positive integers:
-### n → n/2 (n is even)
-### n → 3n + 1 (n is odd)
-### Using the rule above and starting with 13, we generate the following sequence: 13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
-### It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms. 
-### Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
-### Which starting number, under one million, produces the longest chain?
+"""
+Problem 14 - Longest Collatz Sequence
 
-### Solution
+The following iterative sequence is defined for the set of positive integers:
+n → n/2 (n is even)
+n → 3n + 1 (n is odd)
 
-import numpy as np
+Using the rule above and starting with 13, we generate the following sequence: 
+13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
+It can be seen that this sequence (starting at 13 and finishing at 1) contains 
+10 terms. 
 
-# Function to calculate Collatz sequence
-def collatz(n):
-	currentNumber = n
+Although it has not been proved yet (Collatz Problem), it is thought that all 
+starting numbers finish at 1.
+
+Which starting number, under one million, produces the longest chain?
+"""
+
+from typing import Tuple
+
+def collatz(n: int) -> int:
+	"""
+	Parameters
+		n (int): starting number of sequence
+	Returns
+		length (int): length of sequence
+	"""
+
+	curr_n = n
 	length = 0
 
-	while currentNumber != 1:
-		if currentNumber % 2 == 0:
-			currentNumber = currentNumber/2
+	while curr_n != 1:
+		if curr_n % 2 == 0:
+			curr_n /= 2
 		else:
-			currentNumber = 3*currentNumber + 1
+			curr_n = 3*curr_n + 1
 		length += 1
-
-	# Add extra term because loop breaks when the number is 1
-	length += 1
+	length += 1 # Add 1 since look breaks at 1
 	return length
 
-longestChain = 0
-startingNumber = 0
-for i in range(1, 1000000):
-	currentLength = collatz(i)
-	if currentLength > longestChain:
-		longestChain = currentLength
-		startingNumber = i
+def longest_chain(n: int) -> Tuple[int, int]:
+	"""
+	Parameters:
+		n (int): top bound on values to search sequence
+	Returns:
+		longest_chain: length of longest chain below n
+	"""
 
-print('Longest chain: ' + str(longestChain) + ' for starting number of ' + str(startingNumber))
+	longest = 0
+	starting_n = 0
+
+	for i in range(1, n):
+		curr_length = collatz(i)
+		if curr_length > longest:
+			longest = curr_length
+			starting_n = i
+	return longest, starting_n
+
+if __name__ == '__main__':
+	longest, starting_n = longest_chain(1000000)
+	print('Longest chain: ' + str(longest) + ' for starting number of ' +
+		  str(starting_n))
