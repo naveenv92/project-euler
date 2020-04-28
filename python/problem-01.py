b6ce9d69a21f5c6a04affaa9da13e7af38a0ac1a
Naveen Venkatesan <naveen.venkatesan@gmail.com>
2020-04-28 13:09:11 -0700

    Updated problem 1

--- python/problem-01.py
+++ python/problem-01.py
@@ -8,19 +8,19 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 """
 
 def sum_of_multiples(n: int) -> int:
-	"""
-	Parameters
-		n (int): maximum value to use for sum
-	Returns
-		total (int): sum of multiples of 3 and 5
-	"""
+    """
+    Parameters
+        n (int): maximum value to use for sum
+    Returns
+        total (int): sum of multiples of 3 and 5
+    """
 
-	total = 0
-	for i in range(n):
-		if i % 3 == 0 or i % 5 == 0:
-			total += i
-	return total
+    total = 0
+    for i in range(n):
+        if i % 3 == 0 or i % 5 == 0:
+            total += i
+    return total
 
 if __name__ == '__main__':
-	print('The sum of multiples of 3 or 5 below 1000 is: ' +
-		  str(sum_of_multiples(1000)))
+    print('The sum of multiples of 3 or 5 below 1000 is: ' +
+          str(sum_of_multiples(1000)))
