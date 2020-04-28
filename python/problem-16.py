b320f83ac52d2b4ab74190bb0dd0027935984aca
Naveen Venkatesan <naveen.venkatesan@gmail.com>
2020-04-28 13:05:43 -0700

    Updated problem 16

--- python/problem-16.py
+++ python/problem-16.py
@@ -1,14 +1,20 @@
-### Problem 16 - Power Digit Sum
-###-----------------------------------------------------------------
-### 2^(15) = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
-### What is the sum of the digits of the number 2^(1000)?
+"""
+Problem 16 - Power Digit Sum
 
-### Solution
+2^(15) = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26
 
-num = 2**1000
-digitSum = 0
+What is the sum of the digits of the number 2^(1000)?
+"""
 
-for i in str(num):
-	digitSum += int(i)
+def digit_sum(base: int, exponent: int) -> int:
+	"""
+	Parameters
+		base (int): base value
+		exponent (int): exponent to raise base value to
+	"""
+	num = base**exponent
+	return sum([int(i) for i in str(num)])
 
-print('Digit sum: ' + str(digitSum))
+if __name__ == "__main__":
+	print('The digit sum of 2^1000 is: ' + 
+		   str(digit_sum(2, 1000)))
